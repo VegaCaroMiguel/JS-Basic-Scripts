da160e5d5abd9d64407dfc7562fbb6b2ea5bbd1a
Miguel Vega <jm.vegac@uniandes.edu.co>
2024-07-18 06:40:30 -0500

    variables JS

---
+++ utils/variables.js
@@ -0,0 +1,40 @@
+//JavaScript Variables Tutorial 
+
+//var: Reserverd Word for declaring variables 
+//var: function scoped 
+
+//let: Reserved Word for declaring variables 
+//let: Block Scoped 
+
+//const: Reserved Word for declaring constants 
+
+//var samples 
+var x = 5; 
+var y = 6; 
+var z = x + y; 
+
+//const samples 
+const x = 5; 
+const y = 6; 
+const z = x + y; 
+
+//Mixed sample 
+const price1 = 5; 
+const price2 = 6; 
+let total = price1 + price2; 
+
+//Equal operator "="
+
+//JavaScript Data Types 
+
+const pi = 3.14; 
+let person = 'John Doe';
+let answer = 'Yes I am!'; 
+
+//Variables Declaring Only 
+var carName; 
+let carName; 
+
+//Variables value Assignment 
+carName = 'audi'; 
+let carName = 'bmw'; 
