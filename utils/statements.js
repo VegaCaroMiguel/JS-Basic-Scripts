a2120d09a616c25fc52b21bedc005b8e57c7c069
Miguel Vega <jm.vegac@uniandes.edu.co>
2024-07-18 05:52:49 -0500

    Create statements.js

---
+++ utils/statements.js
@@ -0,0 +1,33 @@
+//JavaScript Statement Samples
+//JavaScript is executed by the V8 engine
+//On the Web Browser
+let x, y, z; 
+x = 5; 
+y = 6; 
+z = x + y; 
+
+a=5; b=6; c = a+b; 
+
+document.getElementById('demo').innerHTML = "Hello Dolly.";
+
+let personA = 'Hege';
+let personB='Hege';
+
+//Sample Function 
+
+function myFunction(){
+    document.getElementById('demo1').innerHTML="Hello Dolly!";
+    document.getElementById('demo2').innerHTML="How are you?";
+
+}
+
+//JavaScript reserved words => Keywords
+//var: Declares a variable
+//let: Declares a block variable 
+//const: Declares a block constant
+//if: Declares a block conditional 
+//switch: Declares a block of case conditionals
+//for: Declares a block loop statement
+//function: Declares a function 
+//return: Exits a function 
+//try: Block statement error handling 
